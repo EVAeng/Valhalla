0be87d8e5983cf0d69c0542ad1c98c576c9b0914
Eduardo Luis Santos Delgado <eduardoluissd@gmail.com>
2021-02-14 00:30:48 -0600

    cors

--- src/app.ts
+++ src/app.ts
@@ -7,8 +7,13 @@ const dev: boolean = process.env.NODE_ENV !== 'production';
 
 const app: Application = express();
 const server: http.Server = http.createServer(app);
-const io: Server = new Server();
-io.attach(server);
+const io: Server = new Server(server, {
+ cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  },
+});
+//io.attach(server);
 
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
 	res.status(200).send('Hello, TS, im her');
@@ -18,9 +23,9 @@ io.on('connection', (socket: Socket) => {
 	console.log('connection');
     socket.emit('status', 'Hello from Socket.io');
     
-    socket.on('join-room', (roomId) => {
+    socket.on('join-room', (roomId, userId) => {
         socket.join(roomId);
-        socket.to(roomId).broadcast.emit('user-connected')
+        socket.to(roomId).broadcast.emit('user-connected', userId)
         console.log('joined room')
     })
 
