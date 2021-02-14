d86cc3bce6f69d8b13cc1161a1a2adbfce4f3b88
Eduardo Luis Santos Delgado <eduardoluissd@gmail.com>
2021-02-13 21:19:53 -0600

    basic server and socketio

---
+++ src/app.ts
@@ -0,0 +1,34 @@
+import express, { Application, Request, Response, NextFunction } from 'express';
+import { Server, Socket } from 'socket.io';
+import * as http from 'http';
+
+const port: number = parseInt(process.env.PORT || '5000', 10);
+const dev: boolean = process.env.NODE_ENV !== 'production';
+
+const app: Application = express();
+const server: http.Server = http.createServer(app);
+const io: Server = new Server();
+io.attach(server);
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+	res.status(200).send('Hello, TS, im her');
+});
+
+io.on('connection', (socket: Socket) => {
+	console.log('connection');
+    socket.emit('status', 'Hello from Socket.io');
+    
+    socket.on('join-room', (roomId) => {
+        socket.join(roomId);
+        socket.to(roomId).broadcast.emit('user-connected')
+        console.log('joined room')
+    })
+
+	socket.on('disconnect', () => {
+		console.log('client disconnected');
+	});
+});
+
+server.listen(port, () => {
+	console.log(`> Ready on http://localhost:${port}`);
+});
